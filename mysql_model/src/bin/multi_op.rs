048b716b49ae0e3a203a232661bff5284152ba88
tom <twm@qq.com>
2022-03-30 19:30:17 +0800

    aaa

--- mysql_model/src/bin/multi_op.rs
+++ mysql_model/src/bin/multi_op.rs
@@ -36,6 +36,64 @@ async fn init_pool(
 async fn main() -> Result<(), sqlx::Error> {
     let tz = "+08:00";
     let db_url = "mysql://cf_user:cf123456@192.168.1.26:3306/cf_rs";
+    let pool = init_pool(db_url, tz, 10, 4).await?;
+    let pool = Arc::new(pool);
+
+    let offset = match mysql_util::parse_timezone(tz) {
+        Ok(v) => v,
+        Err(e) => {
+            return Err(MySqxErr(e).into());
+        }
+    };
+
+    let app_begin = Instant::now();
+
+    let mut handles = Vec::new();
+
+    for i in 0..10 {
+        let pool_cl = pool.clone();
+        let offset_cl = offset;
+        let num = i;
+        let handle = tokio::spawn(async move {
+            let begin_t = Instant::now();
+            let v = model::fetch_count(&pool_cl, 100).await;
+            println!(
+                "a, {}, use:{}, result: {:?}",
+                num,
+                begin_t.elapsed().as_millis(),
+                v
+            );
+        });
+        handles.push(handle);
+    }
+
+    for i in 0..10 {
+        let pool_cl = pool.clone();
+        let offset_cl = offset;
+        let num = i;
+        let handle = tokio::spawn(async move {
+            let begin_t = Instant::now();
+            let v = model::fetch_list_unstruct(&pool_cl, 100, &offset_cl).await;
+            println!(
+                "b, {}, use:{}, result: {:?}",
+                num,
+                begin_t.elapsed().as_millis(),
+                v
+            );
+        });
+        handles.push(handle);
+    }
+
+    for h in handles {
+        let _= h.await;
+    }
+    println!("app end. use:{}", app_begin.elapsed().as_millis());
+    Ok(())
+}
+
+async fn main2() -> Result<(), sqlx::Error> {
+    let tz = "+08:00";
+    let db_url = "mysql://cf_user:cf123456@192.168.1.26:3306/cf_rs";
     let pool = init_pool(db_url, tz, 20, 4).await?;
     let pool = Arc::new(pool);
 
